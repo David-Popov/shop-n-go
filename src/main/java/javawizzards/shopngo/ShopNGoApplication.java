48019dc9135f03c3bea5957dcbafb8f0dc1d8171
didicko2003 <didicko2003@gmail.com>
2024-10-01 19:48:24 +0300

    Initial commit

---
+++ src/main/java/javawizzards/shopngo/ShopNGoApplication.java
@@ -0,0 +1,13 @@
+package javawizzards.shopngo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopNGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopNGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/javawizzards/shopngo/ShopNGoApplicationTests.java
@@ -0,0 +1,13 @@
+package javawizzards.shopngo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopNGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
